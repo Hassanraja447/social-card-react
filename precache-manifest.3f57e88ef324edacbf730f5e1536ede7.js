6a6ad9a72eae88282b6431116208847bbf4ac277
Hassan Raja <hassan.uog001@gmail.com>
2019-03-18 09:33:29 +0500

    Updates

---
+++ precache-manifest.3f57e88ef324edacbf730f5e1536ede7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a80822f2b30ab70bd43c",
+    "url": "/hassanraja447.github.io/social-card-react/static/js/runtime~main.a80822f2.js"
+  },
+  {
+    "revision": "3e69a676405d4acfc2df",
+    "url": "/hassanraja447.github.io/social-card-react/static/js/main.3e69a676.chunk.js"
+  },
+  {
+    "revision": "8c0eeccb4f0bae323dd0",
+    "url": "/hassanraja447.github.io/social-card-react/static/js/2.8c0eeccb.chunk.js"
+  },
+  {
+    "revision": "3e69a676405d4acfc2df",
+    "url": "/hassanraja447.github.io/social-card-react/static/css/main.f1e5dcf3.chunk.css"
+  },
+  {
+    "revision": "8a021e57cfcd9acc921414a945cc4fe3",
+    "url": "/hassanraja447.github.io/social-card-react/index.html"
+  }
+];
